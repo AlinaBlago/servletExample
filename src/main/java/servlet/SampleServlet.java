4ce1038f834d0f9a94863845aff579171f5110c1
AlinaBlago <alinablago19@gmail.com>
2020-09-19 21:53:17 +0300

    Initial commit

---
+++ src/main/java/servlet/SampleServlet.java
@@ -0,0 +1,58 @@
+package servlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@WebServlet(name = "sample-servlet", urlPatterns = "/sample")
+public class SampleServlet extends HttpServlet {
+
+     ConcurrentHashMap<String, String> values = new ConcurrentHashMap<>();
+
+    private static final Logger log = LoggerFactory.getLogger(SampleServlet.class);
+
+    @Override
+    public void init() {
+        log.info("Sample Servlet initialized");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        PrintWriter responseBody = resp.getWriter();
+        String ip = req.getRemoteAddr();
+        values.put(ip, req.getHeader("User-agent"));
+        resp.setContentType("text/html");
+        responseBody.println("<h3 align=\"center\">Request from: " + req.getRemoteHost() + "</h3>");
+        responseBody.println(builder(ip));
+
+
+    }
+
+    @Override
+    public void destroy() {
+        log.info("Sample Servlet destroyed");
+    }
+
+    String builder(String ip){
+        StringBuilder builder = new StringBuilder();
+        builder.append("<ul>");
+
+        for (Map.Entry<String, String> stringStringEntry : values.entrySet()){
+            if (stringStringEntry.getKey().equals(ip)){
+                builder.append("<li>").append(stringStringEntry.getValue()).append(" ").append(stringStringEntry.getKey()).append("</li>");
+            }
+        }
+        builder.append("<ul>");
+        return builder.toString();
+    }
+}
